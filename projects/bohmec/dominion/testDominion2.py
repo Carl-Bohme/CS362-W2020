14874963ba559bb00a61d0201e8621105c1bced7
Carl-Bohme <55897133+Carl-Bohme@users.noreply.github.com>
2020-01-19 01:02:56 -0800

    Removed some unnecessary modularization

--- projects/bohmec/dominion/testDominion2.py
+++ projects/bohmec/dominion/testDominion2.py
@@ -35,7 +35,36 @@ trash = []
 players = testUtility.ConstructPlayers(player_names)
 
 #Play the game
-testUtility.Play(supply, supply_order, players, trash)
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1
+    print("\r")
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
 
 #Final score
-testUtility.Score(players)
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
